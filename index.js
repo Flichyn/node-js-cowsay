f5d7c5515b59cf1ca12ded0a61ed41879e635162
Flichyn <nicolasflichy@gmail.com>
2022-04-03 15:27:10 +0200

    Forgot something

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import cowsay from 'cowsay';
 import me from './information.js';
 
 console.log(cowsay.say({
-    text: `Hello, my name is ${firstname + lastname} and my Wild Code School campus is in ${campus}.`
+    text: `Hello, my name is ${me.firstname} ${me.lastname} and my Wild Code School campus is in ${me.campus}.`
 }));
